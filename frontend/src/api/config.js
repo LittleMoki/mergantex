5d2ee5c9ef34e8152e277918a6ff0b8e44156d9e
LittleMoki <steamlittle659@gmail.com>
2024-11-19 23:16:11 +0500

    add

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,7 +1,7 @@
 // local
-export const API_BASE_URL = 'http://localhost:3001/api';
-export const API_BASE_URL_PHOTO = 'http://localhost:3001/uploads';
+// export const API_BASE_URL = 'http://localhost:3001/api';
+// export const API_BASE_URL_PHOTO = 'http://localhost:3001/uploads';
 
 // production
-// export const API_BASE_URL = 'http://212.67.13.166:3001/api'
-// export const API_BASE_URL_PHOTO = 'http://212.67.13.166:3001/uploads'
+export const API_BASE_URL = 'http://212.67.13.166:3001/api'
+export const API_BASE_URL_PHOTO = 'http://212.67.13.166:3001/uploads'
