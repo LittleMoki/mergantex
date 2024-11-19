caad5730c4a544328b48f104959ef3ae901a4dba
LittleMoki <steamlittle659@gmail.com>
2024-11-19 23:15:47 +0500

    change some thinhs

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -3,5 +3,5 @@ export const API_BASE_URL = 'http://localhost:3001/api';
 export const API_BASE_URL_PHOTO = 'http://localhost:3001/uploads';
 
 // production
-// export const API_BASE_URL = 'http://95.130.227.209:3001/api'
-// export const API_BASE_URL_PHOTO = 'http://95.130.227.209:3001/uploads'
+// export const API_BASE_URL = 'http://212.67.13.166:3001/api'
+// export const API_BASE_URL_PHOTO = 'http://212.67.13.166:3001/uploads'
